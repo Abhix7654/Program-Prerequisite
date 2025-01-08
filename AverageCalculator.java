78340e2d3d2d78007e657855523a306e32326aa8
Abhishek Kumar <rajabhishek7654@gmail.com>
2025-01-08 11:10:05 +0530

    AverageCalculator QS-9

---
+++ AverageCalculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AverageCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter First Number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter Second Number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.print("Enter Third Number: ");
+        double num3 = sc.nextDouble();
+
+        double average = (num1 + num2 + num3) / 3;
+
+        System.out.println("Average: " + average);
+    }
+}
